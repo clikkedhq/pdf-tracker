611d814e890984104503255d3268d616d45550d2
clikkedhq <clikkedhq@gmail.com>
2025-04-12 12:50:23 -0700

    Update Document.js

--- backend/models/Document.js
+++ backend/models/Document.js
@@ -1,1 +1,12 @@
-...
+const mongoose = require('mongoose');
+
+const documentSchema = new mongoose.Schema({
+  filename: String,
+  uuid: String,
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Document', documentSchema);
