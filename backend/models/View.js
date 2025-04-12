f59db0357029a1050fdf72cef26f389279b8f498
clikkedhq <clikkedhq@gmail.com>
2025-04-12 12:50:38 -0700

    Update View.js

--- backend/models/View.js
+++ backend/models/View.js
@@ -1,1 +1,10 @@
-...
+const mongoose = require('mongoose');
+
+const viewSchema = new mongoose.Schema({
+  uuid: String,
+  ip: String,
+  userAgent: String,
+  timestamp: Date
+});
+
+module.exports = mongoose.model('View', viewSchema);
