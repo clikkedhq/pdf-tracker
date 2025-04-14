c8ade3e7cc53d7cda82a45cb75b856e7ef9bd9d5
clikkedhq <clikkedhq@gmail.com>
2025-04-14 00:03:57 -0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -31,4 +31,4 @@ app.use('/api/track', analyticsRoutes);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-// Triggering redeploy
+app.use('/api/track', analyticsRoutes);
