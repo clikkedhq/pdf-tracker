8c59224d630677b0a59aa43443139bfd1a949c51
clikkedhq <clikkedhq@gmail.com>
2025-04-22 23:21:16 -0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -19,9 +19,10 @@ app.use('/tracker.js', express.static('public/tracker.js'));
 console.log('Mongo URI:', process.env.MONGO_URI);
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => console.log('MongoDB connected'))
-  .catch((err) => console.error(err));
+  useUnifiedTopology: true
+})
+.then(() => console.log('MongoDB connected'))
+.catch((err) => console.error(err));
 
 // Routes
 app.use('/api/upload', uploadRoutes);
